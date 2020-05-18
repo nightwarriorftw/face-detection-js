7b09f68f4b9a6a45816812fb15f2249c08260bea
nightwarrior-xxx <amanv1999@gmail.com>
2020-05-19 05:23:39 +0530

    Added changes

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ Promise.all([
 
 
 // Detecting the image and labeling
-async function start(){
+async function start() {
   // created container for box
   const container = document.createElement('div');
   container.style.position = 'relative';
@@ -21,27 +21,27 @@ async function start(){
 
   imageUpload.addEventListener('change', async () => {
     const image = await faceapi.bufferToImage(imageUpload.files[0]);
-    
+
     // For drawing boxes on the image
     container.append(image);
     const canvas = faceapi.createCanvasFromMedia(image);
     container.append(canvas);
 
     // changing dimesions of the canvas
-    const dimensions = {width: image.width, height: image.height};
+    const dimensions = { width: image.width, height: image.height };
     faceapi.matchDimensions(canvas, dimensions);
-    
+
 
     const detections = await faceapi.detectAllFaces(image)
-    .withFaceLandmarks().withFaceDescriptors()
+      .withFaceLandmarks().withFaceDescriptors()
 
     // resize all our detections for our dimensions
     const resizedDetections = await faceapi.resizeResults(detections, dimensions);
-    
+
     const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor));
 
     // drawing the actual box for each face
-    results.forEach( (results, i) => {
+    results.forEach((results, i) => {
       const box = resizedDetections[i].detection.box;
       const drawBox = new faceapi.draw.DrawBox(box, {
         label: results.toString()
@@ -52,17 +52,19 @@ async function start(){
 }
 
 // function to parse all the names from the images
-function loadLabeledImages(){
-  const labels = [ 'bittu', 'cute']
+function loadLabeledImages() {
+  const labels = ['jhalani', 'cute']
 
   // return all the promises for returning all the images
   return Promise.all(
-    labels.map( async label => {
+    labels.map(async label => {
       const description = [];
-      for(let i=1;i<=3;i++){
-        const img = faceapi.fetchImage(`<hosted on github>/${label}/${i}.jpeg`);
-        const detections = await faceapi.detectSingleFace()
-        .withFaceLandmarks().withFaceLandmarks();
+      for (let i = 1; i <= 3; i++) {
+        const img = faceapi.fetchImage(
+          `https://github.com/nightwarriorftw/face-detection-js/tree/master/labeled_images/${label}/${i}.jpeg`
+          );
+        const detections = await faceapi.detectSingleFace(img)
+          .withFaceLandmarks().withFaceLandmarks();
         description.push(detections.descriptor);
       }
 
