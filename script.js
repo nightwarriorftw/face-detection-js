d91f25a216d7c9f5268c7d086b3f1518a1a1b073
nightwarrior-xxx <amanv1999@gmail.com>
2020-05-20 16:25:08 +0530

    Added changes

--- script.js
+++ script.js
@@ -74,7 +74,6 @@ function loadLabeledImages() {
           `https://raw.githubusercontent.com/nightwarriorftw/face-detection-js/master/labeled_images/${label}/${i}.jpeg`
           );
         const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
-        console.log(detections);
         descriptions.push(detections.descriptor)
       }
 
